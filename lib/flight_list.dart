dda352fa7be79650989687c3ed7ca73608f74cf6
Dreamwalker <aristojeff@gmail.com>
2018-12-23 00:10:46 +0900

    ff

--- lib/flight_list.dart
+++ lib/flight_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_flight/CustomShapeClipper.dart';
+import 'package:flutter_flight/main.dart';
 import 'package:intl/intl.dart';
 
 final Color discountBackgroundColor = Color(0xFFFFE08D);
@@ -32,11 +33,15 @@ class FlightListing extends StatelessWidget {
           },
         ),
       ),
-      body: Column(
-        children: <Widget>[
-          FlightListTopPart(),
-          FlightListBottomPart(),
-        ],
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: <Widget>[
+            FlightListTopPart(),
+            SizedBox(height: 20.0,),
+            FlightListBottomPart(),
+          ],
+        ),
       ),
     );
   }
@@ -122,14 +127,33 @@ class FlightListBottomPart extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
-          Text(
-            "Best Deals for Next 6 Months",
-            style: dropDownMenuItemsStyle,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Text(
+              "Best Deals for Next 6 Months",
+              style: dropDownMenuItemsStyle,
+            ),
           ),
           SizedBox(
             height: 10.0,
           ),
-          FlightCard(),
+
+          ListView(
+            shrinkWrap: true,
+            physics: BouncingScrollPhysics(),
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              FlightCard(),
+              FlightCard(),
+              FlightCard(),
+              FlightCard(),
+              FlightCard(),
+              FlightCard(),
+              FlightCard(),
+              FlightCard(),
+            ],
+          ),
+
         ],
       ),
     );
@@ -139,58 +163,94 @@ class FlightListBottomPart extends StatelessWidget {
 class FlightCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Container(
-          margin: EdgeInsets.only(right: 16.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10.0)),
-            border: Border.all(color: flightBorderColor),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  children: <Widget>[
-                    Text(
-                      '${formatCurrency.format(4159)}',
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0,
-                      ),
-                    ),
-                    Text(
-                      '${formatCurrency.format(9999)}',
-                      style: TextStyle(
-                          color: Colors.grey,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 16.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+              border: Border.all(color: flightBorderColor),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text(
+                        '${formatCurrency.format(4159)}',
+                        style: TextStyle(
                           fontWeight: FontWeight.bold,
-                          fontSize: 16.0,
-                          decoration: TextDecoration.lineThrough),
-                    )
-                  ],
-                ),
-
-                Row(
-                  children: <Widget>[
-
-                  ],
-                )
-              ],
-
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      SizedBox(
+                        width: 4.0,
+                      ),
+                      Text(
+                        '${formatCurrency.format(9999)}',
+                        style: TextStyle(
+                            color: Colors.grey,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                            decoration: TextDecoration.lineThrough),
+                      )
+                    ],
+                  ),
+                  Wrap(
+                    spacing: 8.0,
+                    runSpacing: -8.0,
+                    children: <Widget>[
+                      FlightDetailChip(Icons.calendar_today, "June 2019"),
+                      FlightDetailChip(Icons.flight_takeoff, "Jet Airways"),
+                      FlightDetailChip(Icons.star, "4.4"),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
-          height: 100.0,
-        ),
-      ],
+          Positioned(
+            top: 10.0,
+            right: 0.0,
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+              child: Text(
+                "55%",
+                style: TextStyle(color: appTheme.primaryColor, fontSize: 14.0, fontWeight: FontWeight.bold),
+              ),
+              decoration: BoxDecoration(
+                  color: discountBackgroundColor,
+                  borderRadius: BorderRadius.all(Radius.circular(10.0),),),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
 
 class FlightDetailChip extends StatelessWidget {
+  final IconData iconData;
+  final String label;
+
+  FlightDetailChip(this.iconData, this.label);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return RawChip(
+      label: Text(label),
+      labelStyle: TextStyle(color: Colors.black, fontSize: 14.0),
+      backgroundColor: chipBackgroundColor,
+      avatar: Icon(
+        iconData,
+        size: 14.0,
+      ),
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10.0))),
+    );
   }
 }
-
